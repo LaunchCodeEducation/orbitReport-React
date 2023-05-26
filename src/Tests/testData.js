8fb42ed221df0215184bf6a36bcba8e7c00bc39e
Courtney Frey <courtneyb.frey@gmail.com>
2023-05-26 14:03:24 -0500

    unit tests added - small css fix

---
+++ src/Tests/testData.js
@@ -0,0 +1,27 @@
+const testData = [
+   {
+     id: 1,
+     name: "Test 1",
+     type: "Test-Comms",
+     launchDate: "1111-11-11",
+     orbitType: "Low",
+     operational: false
+   },
+   {
+     id: 2,
+     name: "Test 2",
+     type: "Test-Position",
+     launchDate: "2222-22-22",
+     orbitType: "Medium",
+     operational: true
+   },
+   {
+     id: 3,
+     name: "Test 3",
+     type: "Test-Probe",
+     launchDate: "3333-33-33",
+     orbitType: "High",
+     operational: false
+   }]
+
+   export default testData;
