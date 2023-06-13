62b0d457409942ec7895d5e359906eac1a411934
Courtney Frey <courtneyb.frey@gmail.com>
2023-06-13 13:06:16 -0500

    added Banner - completed task 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <div>
+      <Banner />
       <Buttons />
       <Table />
       <p>Hello</p>
