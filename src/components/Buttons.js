e0944809661fc8304f3aeb5dc85ce0472f6d8fba
Courtney Frey <courtneyb.frey@gmail.com>
2023-06-13 13:12:55 -0500

    ready to start task 3

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,8 +1,18 @@
-const Buttons = () => {
+import satData from "./satData";
+import "./styling.css";
+
+const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+    // className optional if applying stylings - else plain <div>
+    <div className="flex-container"> 
+      {displaySats.map((sat, id) => {
+        return (
+          <button onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+      })}
+      <button onClick={() => setSat(satData)}>All Orbits</button>
     </div>
   );
 };
