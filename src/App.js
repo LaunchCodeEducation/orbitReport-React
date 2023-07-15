2b02f3213da287a7fa99968821b04bbdff512d52
Sarah <slmetoxen@gmail.com>
2023-07-15 11:49:58 -0500

    Banner.js added and working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
-
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <div>
+      <Banner />
       <Buttons />
       <Table />
     </div>
---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+function Banner(){
+    return(
+        <div>
+            <header>Orbit Report</header>
+            <p>Click on the buttons to see the satellites in that orbit type</p>
+        </div>
+    )
+}
+
+export default Banner;
