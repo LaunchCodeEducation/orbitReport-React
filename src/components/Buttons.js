39b8ac881c57d5709567a4fc1aa4cd8e3f0e4fc3
Sarah <slmetoxen@gmail.com>
2023-07-15 20:37:18 -0500

    Buttons.js updated and looking good!

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,16 @@
-const Buttons = () => {
+import satData from "./satData";
+
+const Buttons = ({filterByType, setSat, displaySats}) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+      {displaySats.map((sat, id) => {
+        return (
+          <button onClick={() => filterByType(sat)} key={id}>{sat} Orbit
+          </button>
+        );
+      })}
+      <button onClick={() => setSat(satData)}>All Orbits</button>
+    </div>
   );
 };
 
