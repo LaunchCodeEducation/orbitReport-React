50cbe9c1a5616c453ede6741e7e7173cdb900aba
Sarah <slmetoxen@gmail.com>
2023-07-16 20:43:51 -0500

    Table.js completed and working!

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,15 +1,29 @@
+import satData from "./satData";
+import App from "../App";
+
 const Table = ({ sat }) => {
   return (
       <table>
        <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
+        {sat.map((data, id) => {
+          return( 
+            <tr key={id}>
+              <td>{data.name}</td>
+              <td>{data.type}</td>
+              <td>{data.launchDate}</td>
+              <td>{`${data.operational === true ? "Active" : "Inactive"}`}</td>
+
+            </tr>
+          )}
+        )} 
         </tbody>
       </table>
   );
