17f829e366f2012742ceb2cbcf6d6ecc3c86e941
Aether1a <olkyrah@outlook.com>
2023-11-21 17:41:10 -0600

    Fixed for workflowV2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import Banner from "./components/Banner"
+import { useState } from "react";
+import satData from "./components/satData"
 
 
 function App() {
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+
+  const filterByType = (currentType) => {
+    const displaySats = satData.filter((newSatDisplay) => {
+      return newSatDisplay.orbitType === currentType;
+    });
+    setSat(displaySats);
+  };
+
   return (
     <div>
-      <Buttons />
-      <Table />
+      <Banner />
+      <Buttons
+        filterByType={filterByType}
+        setSat={setSat}
+        displaySats={displaySats}
+      />
+    <Table sat={sat} />
     </div>
   );
 }
