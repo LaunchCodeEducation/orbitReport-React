042454c7395e954eafb691b9875498bbf4a59543
Aether1a <olkyrah@outlook.com>
2023-11-21 16:47:03 -0600

    final test

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,17 +1,29 @@
+import "./styling.css"
+
 const Table = ({ sat }) => {
   return (
     <table>
     <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
+    <tr>
+      <th>Name</th>
+      <th>Type of Satellite</th>
+      <th>Launch Date</th>
+      <th>Status</th>
+    </tr>
+    </thead>
+    <tbody>
+      {sat.map((data, id) => {
+        return (
+          <tr key={id}>
+            <td>{data.name}</td>
+            <td>{data.type}</td>
+            <td>{data.launchDate}</td>
+            <td>{data.operational ? "active" : "inactive"}</td>
+          </tr>
+          );
+      })}
+    </tbody>
+  </table>
   );
 };
 
